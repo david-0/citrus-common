3ab0fe76e1f40a994a8492311a2526943e416ff0
David Leuenberger <david.leuenberger@hsr.ch>
2017-12-07 07:21:47 +0100

    update interface

--- lib/interfaces/IUserInfo.ts
+++ lib/interfaces/IUserInfo.ts
@@ -3,6 +3,7 @@ import {ICustomerOrder} from "./ICustomerOrder";
 import {IId} from "./IId";
 
 export interface IUserInfo extends IId {
+  number: number;
   email: string;
   name: string;
   prename: string;
