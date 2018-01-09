c8ee8f20537235a6c7d16bdd7e011d9295fa14b1
David Leuenberger <david.leuenberger@hsr.ch>
2018-01-09 07:07:48 +0100

    fix request types

--- lib/interfaces/request/i-request.ts
+++ lib/interfaces/request/i-request.ts
@@ -1,10 +1,9 @@
-import {IOrderDefinition} from "citrus-common";
-import {CModel} from "../model/c/c-model";
-import {IRequestCondition} from "./condition/i-request-condition";
+import {IOrderDefinition} from "./i-order-definition";
+import {IRequestCondition} from "./i-request-condition";
 import {IRequestField} from "./i-request-field";
 
 export interface IRequest<T> {
-  type: typeof CModel | typeof Object;
+  type: typeof Object;
   includedFields?: IRequestField[];
   conditions?: IRequestCondition<T>[];
   order?: IOrderDefinition[];
