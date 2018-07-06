04c6a701f96d6a8bf5e2fc6c1108e0eae16a385b
David Leuenberger <david.leuenberger@hsr.ch>
2018-07-06 17:22:21 +0200

    fix roleDto

--- lib/dto/role-dto.ts
+++ lib/dto/role-dto.ts
@@ -1,23 +1,19 @@
 import {DtoId} from "./dto-id";
-import {UserInfoDto} from "./user-info-dto";
 
 export class RoleDto implements DtoId {
 
   public id?: number;
-  public name: number;
+  public name: string;
 
 
   constructor(role: RoleDto) {
     this.id = role.id;
-    this.userId = role.userId;
-    this.user = role.user;
+    this.name = role.name;
   }
 
   public static createEmpty(): RoleDto {
     return new RoleDto({
-      userId: "",
-      description: "",
-      articleIds: []
+      name: ""
     });
   }
 
