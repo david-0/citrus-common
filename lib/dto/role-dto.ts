b0a0f9767dad56f89b473d38afc274ab77359356
David Leuenberger <david.leuenberger@hsr.ch>
2018-07-10 07:30:28 +0200

    add users to roleDto

--- lib/dto/role-dto.ts
+++ lib/dto/role-dto.ts
@@ -1,14 +1,16 @@
 import {DtoId} from "./dto-id";
+import {UserInfoDto} from "./user-info-dto";
 
 export class RoleDto implements DtoId {
 
   public id?: number;
   public name: string;
-
+  public users?: UserInfoDto;
 
   constructor(role: RoleDto) {
     this.id = role.id;
     this.name = role.name;
+    this.users = role.users;
   }
 
   public static createEmpty(): RoleDto {
