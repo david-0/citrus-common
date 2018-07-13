7ef5ba81477e52de932d4379c699d1ad90deaf78
David Leuenberger <david.leuenberger@hsr.ch>
2018-07-13 15:40:39 +0200

    use article in units

--- lib/dto/unit-of-measurement-dto.ts
+++ lib/dto/unit-of-measurement-dto.ts
@@ -1,3 +1,4 @@
+import {ArticleDto} from "./article-dto";
 import {DtoId} from "./dto-id";
 
 export class UnitOfMeasurementDto implements DtoId {
@@ -5,20 +6,20 @@ export class UnitOfMeasurementDto implements DtoId {
   public id?: number;
   public shortcut: string;
   public description: string;
-  public articleIds: number[];
+  public articles: ArticleDto[];
 
   constructor(unitOfMeasurement: UnitOfMeasurementDto) {
     this.id = unitOfMeasurement.id;
     this.shortcut = unitOfMeasurement.shortcut;
     this.description = unitOfMeasurement.description;
-    this.articleIds = unitOfMeasurement.articleIds;
+    this.articles = unitOfMeasurement.articles;
   }
 
   public static createEmpty(): UnitOfMeasurementDto {
     return new UnitOfMeasurementDto({
       shortcut: "",
       description: "",
-      articleIds: []
+      articles: []
     });
   }
 
