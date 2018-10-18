a2d446b2be97fffc9a0a3b813694bc2ca504e505
David Leuenberger <david.leuenberger@hsr.ch>
2018-10-18 20:13:04 +0200

    add totalPrice to CartDto

--- lib/dto/cart-dto.ts
+++ lib/dto/cart-dto.ts
@@ -1,6 +1,6 @@
 import {CartLocationDto} from "./cart-location-dto";
 
 export class CartDto {
-  constructor(public cartLocations: CartLocationDto[]) {
+  constructor(public cartLocations: CartLocationDto[], public totalPrice: number) {
   }
 }
