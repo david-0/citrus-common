0b29f7beb8393bc271bf1ee5961a295e006b157a
David <david.leuenberger@gmx.ch>
2022-11-04 23:48:25 +0100

    add factory methods

--- lib/dto/message-template-dto.ts
+++ lib/dto/message-template-dto.ts
@@ -2,6 +2,26 @@ import {IDtoId} from "./dto-id";
 
 export class MessageTemplateDto implements IDtoId {
   public id?: number;
-  constructor(public subject: string, public content: string) {
+
+  public static createEmpty(): MessagetTemplateDto {
+    return new MessagetTemplateDto({
+      subject: "",
+      content: "",
+    });
+  }
+
+  static createWithId(id: number, messageTemplateDto: MessageTemplateDto): MessageTemplateDto {
+    const to = new MessageTemplateDto(messageTemplateDto);
+    to.id = id;
+    return to;
+  }
+
+  constructor(public subject: string, public content: string) {    
+  }
+
+  constructor(messageTemplate: MessageTemplateDto) {
+    this.id = messageTemplate.id;
+    this.subject = messageTemplate.subject;
+    this.content = messageTemplate.content;
   }
 }
